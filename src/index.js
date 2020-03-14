7b8634ad1e67b2fa29e11d9cd6b0c8bdb649a9c6
Abimael Andrade <abimaelc.andrade@gmail.com>
2020-03-14 16:58:07 -0300

    Speech synthesiser
    speechSynthesiser

--- src/index.js
+++ src/index.js
@@ -1,9 +1,35 @@
-import "./styles.css";
+/**
+ * References MDN - SpeechSynthesis
+ * https://developer.mozilla.org/pt-BR/docs/Web/API/SpeechSynthesis
+ */
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use Parcel to bundle this sandbox, you can find more info about Parcel
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+const form = document.querySelector("#form-text"),
+  alert = document.querySelector(".alert-suport"),
+  init = () => {
+    form.addEventListener("submit", e => {
+      e.preventDefault();
+
+      let text = document.querySelector("#text");
+      speak(text.value);
+    });
+  },
+  speak = text => {
+    let msg = new SpeechSynthesisUtterance(text);
+    msg.voice = speechSynthesis
+      .getVoices()
+      .filter(voice => voice.lang === "pt-BR")[1];
+    msg.voiceURI = "Google português do Brasil";
+    msg.volume = 1;
+    msg.rate = 0.9;
+    msg.pitch = 1;
+    msg.text = text;
+    msg.lang = "pt-BR";
+
+    speechSynthesis.speak(msg);
+  };
+
+if ("speechSynthesis" in window) {
+  init();
+} else {
+  alert.classList.remove("d-none");
+}
